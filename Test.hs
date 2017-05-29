81be6f6f11b0299dc69ee4972d007f51891e24ff
ASNordroid <n3tstrang3r@yandex.ru>
2017-05-29 05:32:46 +0300

    Add first custom datatype

--- Test.hs
+++ Test.hs
@@ -1,5 +1,6 @@
 module Test where
 
+--functions
 sgn x
     | x >= 0 = 1
     | x < 0 = -1
@@ -9,3 +10,15 @@ fac x
     | x == 0 = 1
 
 primes = filterPrimes[2..100] where filterPrimes (p:xs) = p : filterPrimes [x | x <- xs, x `mod` p /= 0]
+
+--custom datatypes
+data Color = Red | Green | Blue
+
+say :: Color -> String
+say Red   = "You are Red!"
+say Blue  = "You are Blue!"
+say Green = "You are Green!"
+
+--input
+main :: IO ()
+main = putStrLn $ "Hello, sky! " ++ (say Blue) 
