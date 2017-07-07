9d9637d901579600158cc528de5a4d36ba98379f
ASNordroid <n3tstrang3r@yandex.ru>
2017-07-07 18:58:45 +0300

    Add new simple functions

--- Test.hs
+++ Test.hs
@@ -9,7 +9,31 @@ fac x
     | x > 0 = x * fac (x - 1)
     | x == 0 = 1
 
-primes = filterPrimes[2..100] where filterPrimes (p:xs) = p : filterPrimes [x | x <- xs, x `mod` p /= 0]
+primes = filterPrimes [2..100]
+    where
+        filterPrimes [] = []
+        filterPrimes (p:xs) = p : filterPrimes [x | x <- xs, mod x p /= 0]
+
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort [n | n <- xs, n <= x] ++ [x] ++ qsort [n | n <- xs, n > x]
+
+pow :: Integer -> Integer -> Integer
+pow n 0 = 1
+pow n m
+    | even m = pow (n * n) (div m 2)
+    | otherwise = n * pow n (m - 1)
+
+fib :: Int -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+cubesforlist y = map (\x -> x * x * x) y
+
+--conditional exp
+haskell = if 1==1 then "awesome" else "awful"
+
 
 --custom datatypes
 data Color = Red | Green | Blue
@@ -18,6 +42,9 @@ say :: Color -> String
 say Red   = "You are Red!"
 say Blue  = "You are Blue!"
 say Green = "You are Green!"
+
+
+data Maybe a = Nothing | Just a
 
 --input
 main :: IO ()
