7ef1630cc52780d5548fb00e589ef7aa22795eb4
ASNordroid <n3tstrang3r@yandex.ru>
2017-09-24 22:03:51 +0200

    Add transpose function to LinAlg.hs

--- LinAlg.hs
+++ LinAlg.hs
@@ -1,12 +1,9 @@
 module LinAlg where
 
-dota :: Num a => [a] -> [a] -> a
+dot :: Num a => [a] -> [a] -> a
 dota [] [] = 0
 dota (x:xs) (y:ys) = x * y + dot xs ys
 
-dot :: Num a => [a] -> [a] -> a
-dot a b
-    | length a == 0 = 0
-    | length a == length b = head a * head b + dot (tail a) (tail b)
-
-transpose (Ord a) => [a] -> [a]
+transpose :: [[a]] -> [[a]]
+transpose ([]:_) = []
+transpose x = (map head x) : transpose (map tail x)
