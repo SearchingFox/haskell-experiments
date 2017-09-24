2b6814bc45a1595120d5f9464f3539e3a6b8d185
ASNordroid <n3tstrang3r@yandex.ru>
2017-09-24 20:37:50 +0200

    Add a few types and functions

--- Test.hs
+++ Test.hs
@@ -1,9 +1,16 @@
 module Test where
 
---functions
-sgn x
-    | x >= 0 = 1
-    | x < 0 = -1
+-- type
+data Time   = Time Hour Minute Second
+data Hour   = Hour   Int
+data Minute = Minute Int
+data Second = Second Int
+    
+
+-- functions
+sgn a
+    | a < 0 = -1
+    | a >= 0 = 1
 
 fac x
     | x > 0 = x * fac (x - 1)
@@ -19,7 +26,7 @@ qsort [] = []
 qsort (x:xs) = qsort [n | n <- xs, n <= x] ++ [x] ++ qsort [n | n <- xs, n > x]
 
 pow :: Integer -> Integer -> Integer
-pow n 0 = 1
+pow _ 0 = 1
 pow n m
     | even m = pow (n * n) (div m 2)
     | otherwise = n * pow n (m - 1)
@@ -29,13 +36,44 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
-cubesforlist y = map (\x -> x * x * x) y
+cubesforlist y = map (\x -> pow x 3) y
 
---conditional exp
-haskell = if 1==1 then "awesome" else "awful"
+splits :: [a] -> [ ([a], [a]) ]
+splits [] = [ ([], []) ]
+splits (x:xs) = ([], x:xs) : [(x:bs, cs) | (bs, cs) <- splits xs]
+
+perms :: [a] -> [[a]]
+perms [] = [[]]
+perms (x:xs) = [bs ++ x:cs | perm <- perms xs, (bs, cs) <- splits perm]
+
+transpose :: [[a]] -> [[a]]
+transpose ([]:_) = []
+transpose x = (map head x) : transpose (map tail x)
 
 
---custom datatypes
+-- binary tree
+data Tree a = Nil | Branch a (Tree a) (Tree a)
+    deriving (Ord, Eq, Show)
+
+insert :: (Ord a) => Tree a -> a -> Tree a
+insert Nil x = Branch x Nil Nil
+insert (Branch num left right) x
+    | num == x = Branch num left right
+    | num < x  = Branch num left (insert right x)
+    | num > x  = Branch num (insert left x) right
+
+find :: (Ord a) => Tree a -> a -> Bool
+find Nil _ = False
+find (Branch num left right) x
+    | num == x = True
+    | num < x = find right x
+    | num > x = find left x
+
+inorder :: (Ord a) => Tree a -> [a]
+inorder Nil = []
+inorder (Branch num left right) = inorder left ++ [num] ++ inorder right
+
+-- custom datatypes
 data Color = Red | Green | Blue
 
 say :: Color -> String
@@ -43,9 +81,13 @@ say Red   = "You are Red!"
 say Blue  = "You are Blue!"
 say Green = "You are Green!"
 
+conv :: String -> Color
+conv "Blue" = Blue
 
 data Maybe a = Nothing | Just a
 
 --input
-main :: IO ()
-main = putStrLn $ "Hello, sky! " ++ (say Blue) 
+-- main :: IO ()
+-- main = do
+--     color <- getLine
+--     putStrLn $ "Hello, sky! " ++ (say (conv color))
