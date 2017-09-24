85d92c91a2ec5230621b1ab5637124f347e8a553
ASNordroid <n3tstrang3r@yandex.ru>
2017-09-24 20:40:13 +0200

    Add first attempt to write program with input

---
+++ Interactions.hs
@@ -0,0 +1,14 @@
+import Data.List.Split
+
+main :: IO ()
+-- main = interact $ unlines . solve . (map (read :: String -> Int)) . tail . lines
+main = interact $ unlines . (map con) . (splitOn " ")
+
+cube :: Int -> Int
+cube 1 = 1
+cube x = x * x *x
+
+con x = x ++ "-"
+
+solve xs = map show ans
+    where ans = map cube xs
