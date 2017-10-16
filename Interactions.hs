a15427b9bba85662dee14f78ea98885309af30c6
ASNordroid <n3tstrang3r@yandex.ru>
2017-10-16 19:41:16 +0200

    Refactor some files

--- Interactions.hs
+++ Interactions.hs
@@ -1,19 +1,19 @@
 import Data.List.Split
 
---main :: IO ()
+-- main :: IO ()
 -- main = do
 --     color <- getLine
---     putStrLn $ "Hello, sky! " ++ (say (conv color))
+--     putStrLn $ "Hello, sky! You're " ++ color
 
 main :: IO ()
--- main = interact $ unlines . solve . (map (read :: String -> Int)) . tail . lines
 main = interact $ unlines . (map con) . (splitOn " ")
+-- main = interact $ unlines . solve . (map (read :: String -> Int)) . tail . lines
+
+con x = x ++ "-"
 
 cube :: Int -> Int
 cube 1 = 1
 cube x = x * x *x
-
-con x = x ++ "-"
 
 solve xs = map show ans
     where ans = map cube xs
--- Test.hs
+++ Test.hs
@@ -8,30 +8,29 @@ data Minute = Minute Int
 data Second = Second Int
 
 data Color = Red | Green | Blue
-    
+
 
 -- functions
 sgn a
-    | a < 0 = -1
+    | a < 0  = -1
     | a >= 0 = 1
 
 fac x
-    | x > 0 = x * fac (x - 1)
+    | x > 0  = x * fac (x - 1)
     | x == 0 = 1
 
-primes = filterPrimes [2..100]
-    where
-        filterPrimes [] = []
-        filterPrimes (p:xs) = p : filterPrimes [x | x <- xs, mod x p /= 0]
+primes = filterPrimes [2..100] where
+    filterPrimes []     = []
+    filterPrimes (p:xs) = p : filterPrimes [x | x <- xs, mod x p /= 0]
 
 qsort :: (Ord a) => [a] -> [a]
-qsort [] = []
+qsort []     = []
 qsort (x:xs) = qsort [n | n <- xs, n <= x] ++ [x] ++ qsort [n | n <- xs, n > x]
 
 pow :: Integer -> Integer -> Integer
 pow _ 0 = 1
 pow n m
-    | even m = pow (n * n) (div m 2)
+    | even m    = pow (n * n) (div m 2)
     | otherwise = n * pow n (m - 1)
 
 fib :: Int -> Integer
@@ -41,16 +40,31 @@ fib n = fib (n - 1) + fib (n - 2)
 
 cubesforlist y = map (\x -> pow x 3) y
 
-splits :: [a] -> [ ([a], [a]) ]
-splits [] = [ ([], []) ]
-splits (x:xs) = ([], x:xs) : [(x:bs, cs) | (bs, cs) <- splits xs]
+split :: [a] -> [ ([a], [a]) ]
+split []     = [ ([], []) ]
+split (x:xs) = ([], x:xs) : [(x:bs, cs) | (bs, cs) <- split xs]
 
-perms :: [a] -> [[a]]
-perms [] = [[]]
-perms (x:xs) = [bs ++ x:cs | perm <- perms xs, (bs, cs) <- splits perm]
+permutations :: [a] -> [[a]]
+permutations []     = [[]]
+permutations (x:xs) = [bs ++ x:cs | perm <- permutations xs, (bs, cs) <- split perm]
+
+even_dec :: Integral a => [a] -> [a]
+even_dec x = [if y `mod` 2 == 0 then y - 1 else y | y <- x]
+-- even_dec [] = []
+-- even_dec (x:xs) = [x + (x `mod` 2 - 1)] ++ even_dec xs
+
+say :: Color -> String
+say Red   = "You are Red!"
+say Blue  = "You are Blue!"
+say Green = "You are Green!"
+
+conv :: String -> Color
+conv "Blue"  = Blue
+conv "Green" = Blue
+conv "Red"   = Blue
 
 -- bor :: (Num a) => a -> a -> Bool
--- bor a b = or a b
+-- bor a b = a  b
 
 
 -- binary tree
@@ -68,20 +82,9 @@ find :: (Ord a) => Tree a -> a -> Bool
 find Nil _ = False
 find (Branch num left right) x
     | num == x = True
-    | num < x = find right x
-    | num > x = find left x
+    | num < x  = find right x
+    | num > x  = find left x
 
 inorder :: (Ord a) => Tree a -> [a]
 inorder Nil = []
 inorder (Branch num left right) = inorder left ++ [num] ++ inorder right
-
--- custom datatypes
-say :: Color -> String
-say Red   = "You are Red!"
-say Blue  = "You are Blue!"
-say Green = "You are Green!"
-
-conv :: String -> Color
-conv "Blue"  = Blue
-conv "Green" = Blue
-conv "Red"   = Blue
