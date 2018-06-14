7c47a433a22b7381a3d1d3def5a665c59103be71
ASNordroid <n3tstrang3r@yandex.ru>
2018-06-14 23:59:24 +0300

    Add new class and instance

--- PhysicsCalc.hs
+++ PhysicsCalc.hs
@@ -5,31 +5,42 @@ data Area = Length | Force | Time | Momentum | Velocity | Mass
 data Dimension = Dimension Area String
     deriving (Show)
 data Value = Value Double Dimension
-    deriving (Show, Eq)
+    deriving (Eq)
 
--- type Meter = (Area Length) "m"
+-- data, newtype, type
+metre = Dimension Length "m"
+newtone = Dimension Force "N"
+
+class Conversionable a where
+    convertToSI :: a -> a
+    -- convertToAnother :: a -> a
+
+instance Conversionable Value where
+    convertToSI (Value num (Dimension ar dim))
+        | dim == "cm" = Value (num / 100.0) metre
 
 instance Eq Dimension where
     Dimension x _ == Dimension y _ = x == y
 
-instance Read Value where
-    readsPrec num dim =
-        read num :: Double
-        read dim
-        Value num dim
+instance Show Value where
+    show (Value num (Dimension _ dim)) = show num ++ " " ++ dim
+
+-- instance Read Value where
+--     readsPrec _ input = do
+--         let (num, dim) = words input
+--             read num :: Double
+--             in
+--         Value num dim
 
 -- + - * / exp (^) sin cos tan log
-a = Value 5.0 (Dimension Length "m")
-b = Value 6.0 (Dimension Length "m")
+a = Value 5.0 metre
+b = Value 6.0 metre
 
--- infixl :+: 
+-- infixl :+:
 add :: Value -> Value -> Value
 add (Value num1 dim1) (Value num2 dim2)
     | dim1 == dim2 = Value (num1 + num2) dim1
     | otherwise    = error "Dimensions doesn't match"
 
 main :: IO ()
-main = do
-    read a :: Int -- :: Value
-    -- b <- read -- :: Value
-    print $ a + 1
+main = print $ add a b
