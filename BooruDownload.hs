13f08cb55d4a4397f2f3f23b947a4f432bdf5d05
ASNordroid <n3tstrang3r@yandex.ru>
2018-06-18 04:46:57 +0300

    Cleanup BooruDownload.hs

--- BooruDownload.hs
+++ BooruDownload.hs
@@ -1,8 +1,12 @@
--- Download images from booru sites (danbooru, yande.re, etc.)
+-- Download images from booru sites (danbooru.donmai.us, yande.re, etc.)
 -- TODO: add https://konachan.com/
+-- TODO: add working with pages
+-- TODO: add saving to specific directory
+-- TODO: maybe add cli
 {-# LANGUAGE OverloadedStrings #-}
 module Booru where
 
+import Control.Monad
 import Network.HTTP.Req
 import Text.HTML.TagSoup
 import Data.Maybe                               (fromJust)
@@ -12,32 +16,36 @@ import qualified Data.ByteString.Char8 as BS
 instance MonadHttp IO where
     handleHttpException = throwIO
 
-savePicture :: BS.ByteString -> BS.ByteString -> IO ()
-savePicture ext picUrl = req GET (fst $ fromJust $ parseUrlHttps picUrl) NoReqBody bsResponse mempty >>= \pic ->
-    BS.writeFile ("C:\\Users\\Ant\\Desktop\\last_pic." ++ BS.unpack ext) $ responseBody pic
+splitOnChar :: Char -> String -> [String]
+splitOnChar splitOnCharc s = case dropWhile (== c) s of
+                "" -> []
+                s' -> w : splitOnChar c s''
+                    where (w, s'') = break (== c) s'
+
+savePicture :: BS.ByteString -> IO ()
+savePicture picUrl = req GET (fst $ fromJust $ parseUrlHttps picUrl) NoReqBody bsResponse mempty >>= \pic ->
+    BS.writeFile ("C:\\Users\\Ant\\Desktop\\" ++ head (tail fileName) ++ "." ++ last fileName) $ responseBody pic
+        where fileName = splitOnChar '.' $ last $ splitOnChar '/' $ BS.unpack picUrl
 
 -- getProperty :: [Tag BS.ByteString] -> [(BS.ByteString, BS.ByteString)]
 --                                              id           property
 
-getFileUrlY :: [Tag BS.ByteString] -> [BS.ByteString]
-getFileUrlY (x:xs) = case x of
+getFilesUrlY :: [Tag BS.ByteString] -> [BS.ByteString]
+getFilesUrlY (x:xs) = case x of
     TagClose "posts"        -> []
-    TagOpen "post" attrList -> snd (head $ filter (\l -> fst l == "file_url") attrList) : getFileUrlY xs
-    _                       -> getFileUrlY xs
+    TagOpen "post" attrList -> snd (head $ filter (\l -> fst l == "file_url") attrList) : getFilesUrlY xs
+    _                       -> getFilesUrlY xs
 
-getFileUrlD :: [Tag BS.ByteString] -> [BS.ByteString]
-getFileUrlD (x:xs) = case x of -- why (TagOpen x _:y:xs) does not work???
+getFilesUrlD :: [Tag BS.ByteString] -> [BS.ByteString]
+getFilesUrlD (x:xs) = case x of -- why (TagOpen x _:y:xs) does not work???
     TagClose "posts"           -> []
-    TagOpen "large-file-url" _ -> fromTagText (head xs) : getFileUrlD xs
-    _                          -> getFileUrlD xs
-    
+    TagOpen "large-file-url" _ -> fromTagText (head xs) : getFilesUrlD xs
+    _                          -> getFilesUrlD xs
+
 
 main :: IO ()
 main = do
-    -- danbooru - large-file-url -tag
-    -- yandere - file_url -attribute
     let Just (url, options) = parseUrlHttps "https://yande.re/post.xml?tags=liang_xing&page=1"
-    -- let Just (url, options) = parseUrlHttps "https://danbooru.donmai.us/posts.xml?tags=liang_xing&page=1"
     req GET url NoReqBody bsResponse options >>= \r ->
-        print $ length $ getFileUrlY $ parseTags $ responseBody r
-        -- savePicture "jpg" $ getLastImg
+        mapM savePicture $ getFilesUrlY $ parseTags $ responseBody r
+    putStrLn "Done!"
