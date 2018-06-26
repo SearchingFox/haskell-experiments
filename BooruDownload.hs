8588a7b37f4f6e2b3d2288f4b592c28eab85695a
ASNordroid <n3tstrang3r@yandex.ru>
2018-06-26 23:58:11 +0300

    Add cli

--- BooruDownload.hs
+++ BooruDownload.hs
@@ -8,6 +8,7 @@ module Booru (getFilesUrlY, getFilesUrlD) where
 
 import Network.HTTP.Req
 import Text.HTML.TagSoup
+import System.Environment
 import Data.Maybe                               (fromJust)
 import Control.Monad                            (mapM)
 import Control.Exception                        (throwIO)
@@ -17,14 +18,14 @@ instance MonadHttp IO where
     handleHttpException = throwIO
 
 splitOnChar :: Char -> String -> [String]
-splitOnChar splitOnCharc s = case dropWhile (== c) s of
+splitOnChar c s = case dropWhile (== c) s of
                 "" -> []
                 s' -> w : splitOnChar c s''
                     where (w, s'') = break (== c) s'
 
 savePicture :: BS.ByteString -> IO ()
 savePicture picUrl = req GET (fst $ fromJust $ parseUrlHttps picUrl) NoReqBody bsResponse mempty >>= \pic ->
-    BS.writeFile ("C:\\Users\\Ant\\Desktop\\" ++ concat init fileName ++ "." ++ last fileName) $ responseBody pic
+    BS.writeFile ("C:\\Users\\Ant\\Desktop\\" ++ concat (init fileName) ++ "." ++ last fileName) $ responseBody pic
         where fileName = splitOnChar '.' $ last $ splitOnChar '/' $ BS.unpack picUrl
 
 -- getProperty :: [Tag BS.ByteString] -> [(BS.ByteString, BS.ByteString)]
@@ -42,10 +43,34 @@ getFilesUrlD (x:xs) = case x of -- why (TagOpen x _:y:xs) does not work???
     TagOpen "large-file-url" _ -> fromTagText (head xs) : getFilesUrlD xs
     _                          -> getFilesUrlD xs
 
+-- maybe wrap in Maybe
+urlToXmlUrlY :: BS.ByteString -> BS.ByteString
+urlToXmlUrlY url
+    | BS.isInfixOf ".xml?" url = url
+    | BS.isInfixOf "show" url  = BS.pack "https://yande.re/post.xml?tags=id:" <> BS.pack (splitOnChar '/' (BS.unpack url) !! 4)
+    | BS.isInfixOf "post?" url = BS.pack "https://yande.re/post.xml?" <> BS.pack (last (splitOnChar '?' (BS.unpack url)))
+    | otherwise                = url
+
+urlToXmlUrlD :: BS.ByteString -> BS.ByteString
+urlToXmlUrlD url
+    | BS.isInfixOf ".xml" url   = url
+    | BS.isInfixOf "posts/" url = BS.init url <> BS.pack ".xml"
+    | BS.isInfixOf "posts?" url = BS.pack "https://danbooru.donmai.us/posts.xml?" <> BS.pack (last (splitOnChar '?' (BS.unpack url)))
+    | otherwise                 = url
 
 main :: IO ()
 main = do
-    let Just (url, options) = parseUrlHttps "https://yande.re/post.xml?tags=liang_xing&page=1"
-    req GET url NoReqBody bsResponse options >>= \r ->
-        mapM savePicture $ getFilesUrlY $ parseTags $ responseBody r
+    (mode:input) <- BS.words <$> BS.getLine
+    let (url, options) = fromJust $ parseUrlHttps $ if BS.isInfixOf "yande.re" input
+        then urlToXmlUrlY input
+        else urlToXmlUrlD input
+    r <- req GET url NoReqBody bsResponse options
+    let a = if BS.isInfixOf "yande.re" input
+        then getFilesUrlY $ parseTags $ responseBody r
+        else getFilesUrlD $ parseTags $ responseBody r
+    case BS.unpack mode of
+        "p" -> print a -- if BS.isInfixOf "yande.re" input
+        --                 then getFilesUrlY $ parseTags $ responseBody r
+        --                 else getFilesUrlD $ parseTags $ responseBody r
+        "s" -> mapM savePicture a
     putStrLn "Done!"
