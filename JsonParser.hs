8113f1137636d8fbfcb797ef70a3b989458d0c8b
ASNordroid <n3tstrang3r@yandex.ru>
2018-09-01 00:42:49 +0300

    Add small changes to JsonParser.hs

--- JsonParser.hs
+++ JsonParser.hs
@@ -6,17 +6,19 @@ import Data.Aeson.TH
 import Data.Map                                      (Map, toList)
 import Data.Maybe                                    (fromJust)
 import GHC.IO.Encoding
+import System.Directory
 import qualified Data.ByteString.Lazy.Char8 as BL
 
 data WebPage = WebPage { url   :: String
                        , title :: String } deriving Show
 deriveJSON defaultOptions ''WebPage
 
-newtype All = All{windows :: Map String (Map String WebPage)} deriving Show
+newtype All = All { windows :: Map String (Map String WebPage) } deriving Show
 deriveJSON defaultOptions ''All
 
 main :: IO ()
 main = do
-    --setLocaleEncoding utf8 -- or "chcp.com 65001" in console
-    f <- BL.readFile "C:\\Users\\Ant\\Desktop\\Sessions - 10-08-2018 06-05-44.json"
-    writeFile "C:\\Users\\Ant\\Desktop\\Sessionss.txt" (unlines $ map (\l -> url l ++ " " ++ title l) $ concatMap (map snd . toList . snd) $ concatMap (toList . windows) $ fromJust (decode f :: Maybe [All]))
+    setLocaleEncoding utf8 -- or "chcp.com 65001" in console
+    homeDir <- getHomeDirectory
+    file <- BL.readFile $ homeDir ++ "\\Desktop\\Sessions - 29-08-2018 20-13-36.json"
+    writeFile (homeDir ++ "\\Desktop\\Sessions.txt") (unlines $ map (\l -> url l ++ " " ++ title l) $ concatMap (map snd . toList . snd) $ concatMap (toList . windows) $ fromJust (decode file :: Maybe [All]))
