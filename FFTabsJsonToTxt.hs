debf62d74283faed79cf694a46a13f376bc0cc37
ASNordroid <n3tstrang3r@yandex.ru>
2018-10-09 23:05:54 +0300

    Add deduplicate function to FFTabsJsonToTxt

--- FFTabsJsonToTxt.hs
+++ FFTabsJsonToTxt.hs
@@ -16,15 +16,21 @@ deriveJSON defaultOptions ''WebPage
 newtype All = All { windows :: Map String (Map String WebPage) } deriving Show
 deriveJSON defaultOptions ''All
 
+deduplicate :: [String] -> [String]
+deduplicate [] = []
+deduplicate (x:xs)
+    | x `elem` xs = deduplicate xs
+    | otherwise = x : deduplicate xs
+
 main :: IO ()
 main = do
     setLocaleEncoding utf8 -- or "chcp.com 65001" in console
     homeDir <- getHomeDirectory
     file    <- BL.readFile $ homeDir ++ "\\Desktop\\Sessions - 29-08-2018 20-13-36.json"
 
-    writeFile (homeDir ++ "\\Desktop\\FirefoxSavedTabsExport.txt") $ unlines $
-        map ( \l -> case title l of
-                Nothing -> url l ++ "\n" ++ "None"
-                _       -> url l ++ "\n" ++ fromJust (title l) ) $
-                    concatMap (map snd . toList . snd) $
-                    concatMap (toList . windows) $ fromJust (decode file :: Maybe [All])
+    writeFile (homeDir ++ "\\Desktop\\SessionsFromHaskell.txt") $ unlines $ deduplicate $
+        -- map ( \l -> case title l of
+        --         Nothing -> url l ++ "\n" ++ "None"
+        --         _       -> url l ++ "\n" ++ fromJust (title l) ) $
+        map url $ concatMap (map snd . toList . snd) $
+            concatMap (toList . windows) $ fromJust (decode file :: Maybe [All])
