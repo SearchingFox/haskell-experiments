9c42d92432c9377aad325201f42e055e28ca915d
ASNordroid <n3tstrang3r@yandex.ru>
2019-09-01 23:11:16 +0300

    Update BayesBookmarks.hs

--- BayesBookmarks.hs
+++ BayesBookmarks.hs
@@ -1,29 +1,43 @@
 module BayesBookmarks where
 
-import qualified Data.Vector as V
+import Data.Char
 import Control.Monad
 import Data.Map (Map)
 import qualified Data.Map as M
+import qualified Data.Vector as V
 
 -- read line in file
 -- split line for words
 -- head is class
 -- tail is all words that should be added to class
 
-data Tst = Tst (Map String Double)
-data Tag = Tag (Map String Tst)
+type Wrd  = String
+type Class = String
+type Probability = Double
+data Tst = Tst (Map Class Probability)  -- ? type
+data Tag = Tag (Map Wrd Tst)
 
-makeTag = undefined
+-- filter popular words
+f :: String -> String
+f s@(c:cs)
+    | cs == [] = []
+    | c == ' ' = ' ' : f cs
+    | isLetter c = toLower c : f cs
+    | otherwise = ' ' : f cs
+--filter (not . (`elem` ",.?!-:;\"\'"))
+makeTag :: [String] -> [(String, Int)]
+makeTag [] = []
+makeTag (x:xs) = (f x, 0) : makeTag xs
 
-train :: [String] -> [Tags]
-train (x:xs)
-    | xs == [] = []
-    | otherwise = makeTag (take num xs) ++ train (drop num xs)
-        where
-            t = words $ head lns
-            cl = head t
-            num = read (tail t) :: Int
+-- ? ByteString
+train :: [String] -> [(String, [(String, Int)])]
+train [] = []
+train (x:xs) = (cl, makeTag (take num xs)) : train (drop num xs)
+    where
+        t  = words x
+        cl = head t
+        num = read (last t) :: Int
 
 main :: IO ()
-main = readFile "C:\\Users\\Asus\\Desktop\\dataset.txt" >>= \t -> putStrLn $ head $ lines t
+main = readFile "C:\\Users\\Asus\\Desktop\\dataset1.txt" >>= \t -> print $ train $ lines t
     
